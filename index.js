9c71e948b74f4e9ae5fd4e2fb616179f9696935c
Joellehelm <joellenhelm@gmail.com>
2019-11-07 10:59:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+
+function superbowlWin(array){
+  let found = array.find(obj => obj.result === "W")
+  return !!found ? found.year : undefined
+
 }
+
+
